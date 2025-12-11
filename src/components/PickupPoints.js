9091df4aa41e29d749ac0dff03e21f4d085a5d5b
Oniel Milian <onimilian28@gmail.com>
2025-12-11 03:20:38 -0500

    u/points

--- src/components/PickupPoints.js
+++ src/components/PickupPoints.js
@@ -192,7 +192,7 @@ const PickupPoints = () => {
             <li>Si deseas entrega a domicilio, completa los datos</li>
             <li>Entrega gratis en zonas cercanas de 8AM-12M</li>
             <li>Paga al recibir tus productos en la puerta de tu casa</li>
-            <li>¡Disfruta de alimentos directos del campo a tu mesa!</li>
+            <li>¡Disfruta de productos de limpieza de calidad!</li>
           </ol>
         </div>
       </div>
